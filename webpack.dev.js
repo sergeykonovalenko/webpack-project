904133f601bb06c62c2fcc124c59b1b4241a37b0
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2021-10-21 22:57:37 +0300

    Migrating from webpack-dev-server v3 to v4

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,46 +2,30 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const chokidar = require('chokidar');
 const webpack = require('webpack');
-
-// Get local IP address in Node.js
-let os = require('os');
-let ifaces = os.networkInterfaces();
-let localIpAddress;
-
-Object.keys(ifaces).forEach(function (ifname) {
-    ifaces[ifname].forEach(function (iface) {
-        if ('IPv4' !== iface.family || iface.internal !== false) {
-            return;
-        }
-        localIpAddress = iface.address;
-    });
-});
 
 module.exports = (env, argv) => {
 
     return merge(common(argv.mode), {
-        target: 'web',
         devtool: 'eval-cheap-module-source-map',
         devServer: {
-            before(app, server) {
-                chokidar.watch([
-                    './src/**/*.html'
-                ]).on('all', () => {
-                    server.sockWrite(server.sockets, 'content-changed');
-                });
+            devMiddleware: {
+                writeToDisk: true,
             },
-            // contentBase: './dist',
-            contentBase: path.join(__dirname, 'dist'),
-            overlay: {
-                warnings: true,
-                errors: true
+            static: {
+                // directory: path.resolve(__dirname, 'dist'),
+                // directory: './dist',
             },
-            host: localIpAddress,
-            port: 8081,
-            hot: true,
-            // writeToDisk: true,
+            watchFiles: ['src/**/*.html', 'public/**/*'],
+            client: {
+                overlay: {
+                    errors: true,
+                    warnings: true,
+                },
+            },
+            host: 'local-ip',
+            port: 8081, // 8081, 'auto'
+            hot: true, // true, 'only'
         },
         watchOptions: {
             aggregateTimeout: 100,
