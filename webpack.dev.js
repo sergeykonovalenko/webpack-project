0c16d18d18c2f40dceb49a98b7dab106b9d88cfb
Serhii Konovalenko <sergeykonovalenko5550199@gmail.com>
2026-05-18 23:08:46 +0300

    feat: implement project-wide linting configuration with ESLint and update build scripts

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,8 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const path = require('path');
+// const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const webpack = require('webpack');
+// const webpack = require('webpack');
 
 module.exports = (env, argv) => {
   return merge(common(argv.mode), {
